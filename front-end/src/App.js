f18ad44e41adec80eeba346ee202483176b1741e
5935512009 <Pasut2541@hotmail.com>
2024-10-01 09:32:48 +0700

    Initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hero from './components/hero/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      VesShop
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
